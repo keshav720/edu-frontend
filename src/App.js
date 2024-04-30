db28f0657a9dbdd96cb1ef22abac93ee7c149969
keshav720 <keshav9521797178@gmail.com>
2024-04-30 10:08:48 +0530

    intial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red">
+    Educational Website ...
+    <h1 className="text-3xl font-bold underline">
+      Hello students!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
