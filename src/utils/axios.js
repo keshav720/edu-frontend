e8f3938927d8e4cb062bbabd4d0f430e351b83eb
keshav720 <keshav9521797178@gmail.com>
2024-05-07 13:47:11 +0530

    axios url changed

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 const baseUrl = process.env.REACT_APP_BASE_LOCAL_URL;
 console.log("base----", baseUrl);
 export const axiosInstance = axios.create({
-  baseURL: `http://localhost:5000/api/v1`,
+  baseURL: `https://eduapp.zapto.org/api/v1`,
 });
