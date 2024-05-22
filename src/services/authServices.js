12b3fd4c7d6baa57c77b75fab97008de79be11ea
keshav720 <keshav9521797178@gmail.com>
2024-05-22 14:06:10 +0530

    auth service code fix

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -2,7 +2,7 @@ import { axiosInstance } from "../utils/axios";
 export const signUp = async (userData) => {
     try {
       
-      const response = await axiosInstance.post('/users/signup', userData);
+      const response = await axiosInstance.post('/auth/signup', userData);
       
       return response.data;
     } catch (error) {
@@ -13,7 +13,7 @@ export const signUp = async (userData) => {
   
   export const login = async (userData) => {
     try {
-      const response = await axiosInstance.post('/users/login', userData);
+      const response = await axiosInstance.post('/auth/login', userData);
       return response.data;
     } catch (error) {
       console.log("Error in login---",error);
